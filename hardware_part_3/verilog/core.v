1734110374144f863e0fe453572bcbb909d5ac4a
Christopher Hughes <clhughes@ucsd.edu>
2024-12-11 19:58:00 -0800

    test

--- hardware_part_3/verilog/core.v
+++ hardware_part_3/verilog/core.v
@@ -6,26 +6,29 @@ module core #(
     parameter num = 2048
 )(
     input clk,
-    input reset, 
+    input reset,
     input [33:0] inst,
     input [bw*row-1:0] D_xmem,
     output valid,
     output [psum_bw*col-1:0] coreOut
 );
 
-wire [psum_bw*col-1:0] ofifoOut;
+// Mode select signal (inst[7])
+// For now, we explicitly acknowledge it but do nothing with it.
+wire mode_select;
+assign mode_select = inst[7]; // 0 = WS (Weight Stationary), 1 = OS (Output Stationary)
 
-wire xMemWEN;
-wire xMemCEN;
-wire [10:0]xMemAddress;
+// --- Signals for XMem ---
+wire xMemWEN, xMemCEN;
+wire [10:0] xMemAddress;
 wire [bw*row-1:0] xMemOut;
 
 assign xMemCEN = inst[19];
 assign xMemWEN = inst[18];
 assign xMemAddress = inst[17:7];
 
-wire psumMemWEN;
-wire psumMemCEN;
+// --- Signals for PSUM memory (if needed later) ---
+wire psumMemWEN, psumMemCEN;
 wire [psum_bw*col-1:0] psumMemOut;
 wire [10:0] psumMemAddress;
 
@@ -33,18 +36,19 @@ assign psumMemWEN = inst[31];
 assign psumMemCEN = inst[32];
 assign psumMemAddress = inst[30:20];
 
-//Instantiate corelet
-corelet #(.row(row),.col(col),.psum_bw(psum_bw),.bw(bw)) corelet (
+// --- Instantiate Corelet ---
+corelet #(.row(row), .col(col), .psum_bw(psum_bw), .bw(bw)) corelet_instance (
     .clk(clk),
     .reset(reset),
     .inst(inst),
     .coreletIn(xMemOut),
-    .psumIn(ofifoOut),
-    .sfpIn(psumMemOut),
+    .psumIn({psum_bw*col{1'b0}}), // Hardcoded zero for now
+    .sfpIn({psum_bw*col{1'b0}}),  // Hardcoded zero for now
     .sfpOut(coreOut)
 );
 
-sram_32b_w2048 #(.num(num)) xMem (
+// --- Instantiate XMem ---
+sram_32b_w2048 #(.num(num)) xMem_instance (
     .CLK(clk),
     .WEN(xMemWEN),
     .CEN(xMemCEN),
@@ -53,11 +57,12 @@ sram_32b_w2048 #(.num(num)) xMem (
     .Q(xMemOut)
 );
 
-sram_128b_w2048 #(.num(num)) psumMem (
+// --- Instantiate PSUM memory ---
+sram_128b_w2048 #(.num(num)) psumMem_instance (
     .CLK(clk),
     .WEN(psumMemWEN),
     .CEN(psumMemCEN),
-    .D(ofifoOut),
+    .D(coreOut),
     .A(psumMemAddress),
     .Q(psumMemOut)
 );
