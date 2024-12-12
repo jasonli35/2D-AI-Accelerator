72a5c07289ca6a9e228bcfd6f16bb1dd33f0d7d7
Christopher Hughes <clhughes@ucsd.edu>
2024-12-11 20:49:03 -0800

    test

--- hardware_part_3/verilog/corelet.v
+++ hardware_part_3/verilog/corelet.v
@@ -8,12 +8,13 @@ module corelet #(
     input reset,
     input [34:0] inst,
     input [bw*row-1:0] coreletIn,
-    output [psum_bw*col-1:0] psumIn,
+    input mode_select, // Add mode_select as an input
     input [psum_bw*col-1:0] sfpIn,
+    output [psum_bw*col-1:0] psumIn,
     output [psum_bw*col-1:0] sfpOut
 );
 
-//L0 signals
+// --- L0 Signals ---
 wire l0_wr;
 wire l0_rd;
 wire [bw*row-1:0] l0_out;
@@ -22,7 +23,6 @@ wire l0_ready;
 
 assign l0_wr = inst[2];
 assign l0_rd = inst[3];
-
 
 l0 #(.row(row), .bw(bw)) L0_instance (
     .clk(clk),
@@ -35,27 +35,26 @@ l0 #(.row(row), .bw(bw)) L0_instance (
     .o_ready(l0_ready)
 );
 
+// --- MAC Array Signals ---
 wire [psum_bw*col-1:0] macArrayOut;
 wire [1:0] macArrayInst;
 wire [col-1:0] valid;
-wire [psum_bw*col-1:0] macArrayIn_n;
+wire [psum_bw*col-1:0] macArrayIn;
 
 assign macArrayInst = inst[1:0];
-assign macArrayIn_n = 0;
-assign macArrayIn = l0_out;
+assign macArrayIn = mode_select ? sfpIn : {psum_bw*col{1'b0}}; // OS uses sfpIn, WS uses 0
 
-mac_array #(.bw(bw),.psum_bw(psum_bw),.col(col),.row(row)) mac_array (
+mac_array #(.bw(bw), .psum_bw(psum_bw), .col(col), .row(row)) mac_array (
     .clk(clk),
     .reset(reset),
     .out_s(macArrayOut),
     .in_w(l0_out),
     .inst_w(macArrayInst),
-    .in_n(macArrayIn_n),
+    .in_n(macArrayIn),
     .valid(valid)
 );
 
-//OFIFO signals
-
+// --- OFIFO Signals ---
 wire ofifo_rd;
 wire [psum_bw*col-1:0] ofifo_in;
 wire [psum_bw*col-1:0] ofifo_out;
@@ -65,9 +64,8 @@ wire ofifo_valid;
 
 assign ofifo_rd = inst[6];
 assign ofifo_in = macArrayOut;
-assign psumIn = ofifo_out;
+assign psumIn = mode_select ? ofifo_out : {psum_bw*col{1'b0}}; // OS uses ofifo_out, WS uses 0
 
-//Instantiate OFIFO
 ofifo #(.col(col), .psum_bw(psum_bw)) ofifo_instance (
     .clk(clk),
     .reset(reset),
@@ -80,6 +78,7 @@ ofifo #(.col(col), .psum_bw(psum_bw)) ofifo_instance (
     .o_valid(ofifo_valid)
 );
 
+// --- SFP Signals ---
 wire sfp_acc;
 wire sfp_relu;
 wire [psum_bw*col-1:0] sfp_in;
@@ -87,18 +86,19 @@ wire [psum_bw*col-1:0] sfp_out;
 
 assign sfp_acc = inst[33];
 assign sfp_relu = 0;
+assign sfp_in = mode_select ? {psum_bw*col{1'b0}} : psumIn; // WS uses psumIn, OS uses 0
 assign sfpOut = sfp_out;
 
-//Instantiate SFP
+// Instantiate SFP
 genvar i;
-for (i=1; i<col+1; i=i+1) begin : sfp_num
+for (i = 1; i < col + 1; i = i + 1) begin : sfp_num
     sfp #(.psum_bw(psum_bw)) sfp_instance (
         .clk(clk),
         .acc(sfp_acc),
         .relu(sfp_relu),
         .reset(reset),
-        .in(sfpIn[psum_bw*i-1:psum_bw*(i-1)]),
-        .out(sfp_out[psum_bw*i-1:psum_bw*(i-1)])
+        .in(sfp_in[psum_bw * i - 1 : psum_bw * (i - 1)]),
+        .out(sfp_out[psum_bw * i - 1 : psum_bw * (i - 1)])
     );
 end
 
