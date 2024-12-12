aa616350ff95aa4fcbb90fe00a4cc63acd447737
Christopher Hughes <clhughes@ucsd.edu>
2024-12-11 21:00:09 -0800

    test

--- hardware_part_3/verilog/corelet.v
+++ hardware_part_3/verilog/corelet.v
@@ -6,13 +6,16 @@ module corelet #(
 )(
     input clk,
     input reset,
-    input [34:0] inst,
+    input [34:0] inst, // Expanded to include mode_select
     input [bw*row-1:0] coreletIn,
     output [psum_bw*col-1:0] psumIn,
     input [psum_bw*col-1:0] sfpIn,
-    output [psum_bw*col-1:0] sfpOut,
-    input mode_select // Added but unused
+    output [psum_bw*col-1:0] sfpOut
 );
+
+// Mode selection signal (added but unused for now)
+wire mode_select;
+assign mode_select = inst[34]; // Pass-through only, no logic changes
 
 // L0 signals
 wire l0_wr;
@@ -35,12 +38,14 @@ l0 #(.row(row), .bw(bw)) L0_instance (
     .o_ready(l0_ready)
 );
 
-// MAC Array signals
 wire [psum_bw*col-1:0] macArrayOut;
 wire [1:0] macArrayInst;
 wire [col-1:0] valid;
+wire [psum_bw*col-1:0] macArrayIn_n;
 
 assign macArrayInst = inst[1:0];
+assign macArrayIn_n = 0;
+assign macArrayIn = l0_out;
 
 mac_array #(.bw(bw), .psum_bw(psum_bw), .col(col), .row(row)) mac_array (
     .clk(clk),
@@ -48,7 +53,7 @@ mac_array #(.bw(bw), .psum_bw(psum_bw), .col(col), .row(row)) mac_array (
     .out_s(macArrayOut),
     .in_w(l0_out),
     .inst_w(macArrayInst),
-    .in_n({psum_bw*col{1'b0}}), // Default to zero (no mode_select impact)
+    .in_n(macArrayIn_n),
     .valid(valid)
 );
 
@@ -56,6 +61,9 @@ mac_array #(.bw(bw), .psum_bw(psum_bw), .col(col), .row(row)) mac_array (
 wire ofifo_rd;
 wire [psum_bw*col-1:0] ofifo_in;
 wire [psum_bw*col-1:0] ofifo_out;
+wire ofifo_full;
+wire ofifo_ready;
+wire ofifo_valid;
 
 assign ofifo_rd = inst[6];
 assign ofifo_in = macArrayOut;
@@ -68,17 +76,31 @@ ofifo #(.col(col), .psum_bw(psum_bw)) ofifo_instance (
     .rd(ofifo_rd),
     .in(ofifo_in),
     .out(ofifo_out),
-    .o_full(),
-    .o_ready(),
-    .o_valid()
+    .o_full(ofifo_full),
+    .o_ready(ofifo_ready),
+    .o_valid(ofifo_valid)
 );
 
-// SFP signals
 wire sfp_acc;
 wire sfp_relu;
+wire [psum_bw*col-1:0] sfp_in;
+wire [psum_bw*col-1:0] sfp_out;
 
 assign sfp_acc = inst[33];
 assign sfp_relu = 0;
-assign sfpOut = sfpIn; // Direct pass-through for now (no mode_select impact)
+assign sfpOut = sfp_out;
+
+// Instantiate SFP
+genvar i;
+for (i = 1; i < col + 1; i = i + 1) begin : sfp_num
+    sfp #(.psum_bw(psum_bw)) sfp_instance (
+        .clk(clk),
+        .acc(sfp_acc),
+        .relu(sfp_relu),
+        .reset(reset),
+        .in(sfpIn[psum_bw * i - 1 : psum_bw * (i - 1)]),
+        .out(sfp_out[psum_bw * i - 1 : psum_bw * (i - 1)])
+    );
+end
 
 endmodule
