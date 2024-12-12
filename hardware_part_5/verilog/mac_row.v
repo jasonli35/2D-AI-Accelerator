6b0737d5cee4ca6531939c333b115b0896b58f55
Christopher Hughes <clhughes@ucsd.edu>
2024-12-11 23:17:18 -0800

    test

--- hardware_part_5/verilog/mac_row.v
+++ hardware_part_5/verilog/mac_row.v
@@ -1,36 +1,36 @@
-module mac_row (clk, out_s, in_w, in_n, valid, inst_w, mode_select, reset);
+module mac_row (
+    clk, out_s, in_w, in_n, valid, inst_w, reset, row_out
+);
 
-    parameter bw = 4;
-    parameter psum_bw = 16;
-    parameter col = 8;
+parameter bw = 4;
+parameter psum_bw = 16;
+parameter col = 8;
 
-    input  clk, reset, mode_select;
-    output [psum_bw*col-1:0] out_s;
-    output [col-1:0] valid;
-    input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
-    input  [1:0] inst_w;
-    input  [psum_bw*col-1:0] in_n;
+input  clk, reset;
+output [psum_bw*col-1:0] out_s;
+output [col-1:0] valid;
+input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
+input  [1:0] inst_w;
+input  [psum_bw*col-1:0] in_n;
+output [psum_bw*col-1:0] row_out; // New output for collecting all tile results
 
-    wire [(col+1)*bw-1:0] temp_in;
-    wire [(col+1)*2-1:0] temp_inst;
+wire [psum_bw-1:0] tile_outputs [0:col-1]; // Collect individual tile outputs
+assign row_out = {tile_outputs[col-1], tile_outputs[col-2], ..., tile_outputs[0]}; // Concatenate all tile outputs
 
-    assign temp_in[bw-1:0]  = in_w;
-    assign temp_inst[1:0]   = inst_w;
-
-    genvar i;
-    for (i=1; i < col+1; i=i+1) begin : col_num
-        mac_tile #(.bw(bw), .psum_bw(psum_bw)) mac_tile_instance (
-            .clk(clk),
-            .reset(reset),
-            .mode_select(mode_select), // Pass mode_select to each tile
-            .in_w(temp_in[bw*i-1:bw*(i-1)]),
-            .out_e(temp_in[bw*(i+1)-1:bw*i]),
-            .inst_w(temp_inst[2*i-1:2*(i-1)]),
-            .inst_e(temp_inst[2*(i+1)-1:2*i]),
-            .in_n(in_n[(psum_bw*i)-1:psum_bw*(i-1)]),
-            .out_s(out_s[(psum_bw*i)-1:psum_bw*(i-1)])
-        );
-        assign valid[i-1] = temp_inst[2*(i+1)-1];
-    end
+genvar i;
+for (i = 0; i < col; i = i + 1) begin : mac_tiles
+    mac_tile #(.bw(bw), .psum_bw(psum_bw)) mac_tile_instance (
+        .clk(clk),
+        .reset(reset),
+        .in_w(in_w),
+        .out_e(),
+        .in_n(in_n[psum_bw*(i+1)-1:psum_bw*i]),
+        .inst_w(inst_w),
+        .inst_e(),
+        .mode_select(),
+        .out_s(out_s[psum_bw*(i+1)-1:psum_bw*i]),
+        .tile_out(tile_outputs[i]) // Forward tile output
+    );
+end
 
 endmodule
