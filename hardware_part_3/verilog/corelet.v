1afb64bc8840ef84523465bf04eab94032d662f1
Christopher Hughes <clhughes@ucsd.edu>
2024-12-11 21:17:31 -0800

    test

--- hardware_part_3/verilog/corelet.v
+++ hardware_part_3/verilog/corelet.v
@@ -6,7 +6,7 @@ module corelet #(
 )(
     input clk,
     input reset,
-    input [34:0] inst, // Includes mode_select
+    input [34:0] inst, // Expanded to include mode_select
     input [bw*row-1:0] coreletIn,
     output [psum_bw*col-1:0] psumIn,
     input [psum_bw*col-1:0] sfpIn,
@@ -15,7 +15,7 @@ module corelet #(
 
 // Mode selection signal
 wire mode_select;
-assign mode_select = inst[34];
+assign mode_select = inst[34]; // 0: WS, 1: OS
 
 // L0 signals
 wire l0_wr;
@@ -43,9 +43,9 @@ wire [1:0] macArrayInst;
 wire [col-1:0] valid;
 wire [psum_bw*col-1:0] macArrayIn_n;
 
-// MAC Array input connections
+// MAC Array input selection
 assign macArrayInst = inst[1:0];
-assign macArrayIn_n = (mode_select) ? psumIn : {psum_bw*col{1'b0}}; // OS: psumIn, WS: zero
+assign macArrayIn_n = (mode_select == 1) ? psumIn : {psum_bw*col{1'b0}}; // OS uses psumIn, WS zeros out
 
 // Instantiate MAC Array
 mac_array #(.bw(bw), .psum_bw(psum_bw), .col(col), .row(row)) mac_array (
@@ -70,7 +70,7 @@ assign ofifo_rd = inst[6];
 assign ofifo_in = macArrayOut;
 
 // Assign psumIn based on mode_select
-assign psumIn = (mode_select) ? ofifo_out : macArrayOut; // OS: ofifo_out, WS: direct MAC output
+assign psumIn = (mode_select == 1) ? ofifo_out : macArrayOut; // OS: ofifo_out, WS: direct MAC output
 
 ofifo #(.col(col), .psum_bw(psum_bw)) ofifo_instance (
     .clk(clk),
@@ -84,13 +84,14 @@ ofifo #(.col(col), .psum_bw(psum_bw)) ofifo_instance (
     .o_valid(ofifo_valid)
 );
 
+// SFP signals
 wire sfp_acc;
 wire sfp_relu;
 wire [psum_bw*col-1:0] sfp_in;
 wire [psum_bw*col-1:0] sfp_out;
 
 // SFP input selection based on mode_select
-assign sfp_in = (mode_select) ? psumIn : sfpIn; // OS: psumIn, WS: original sfpIn
+assign sfp_in = (mode_select == 1) ? psumIn : sfpIn; // OS: psumIn, WS: original sfpIn
 
 assign sfp_acc = inst[33];
 assign sfp_relu = 0;
